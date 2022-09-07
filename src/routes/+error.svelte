75b045adf4c6aa7038c39c7e7ba91c1eb45524f9
vanauss <me@jakubmuller.cz>
2022-09-07 13:28:30 +0200

    Initial App

---
+++ src/routes/+error.svelte
@@ -0,0 +1,32 @@
+<script>
+    import { page } from '$app/stores';
+  </script>
+
+
+
+<div class="error">
+    <h1>Toto se tady bohužel nenechází :( </h1>
+  
+    <p>chyba: {$page.status}</p>
+</div>
+<style>
+    * {
+    font-family: 'Quicksand', sans-serif;
+}
+
+   .error {
+        height: 10em;
+        width:50em;
+        background-color: #3697a7;
+        display:flex;
+        flex-direction: column;
+        justify-content: center;;
+        align-items: center;
+        margin: 0 auto;
+        margin-top: 10em;
+        margin-bottom: 10em;
+        color: white;
+    }
+
+   
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,89 @@
+<nav>
+    <a href="/">home</a>
+    <a href="/">about me</a>
+    <a href="/">my work</a>
+</nav>
+
+<slot></slot>
+
+<footer> © Jack Jackson 2020</footer>
+
+
+<style>
+* {
+    font-family: 'Quicksand', sans-serif;
+}
+
+nav {
+    background-color: #1f464d;
+    display:flex;
+    flex-direction: row-reverse;
+    padding: 2em 10em;
+}    
+
+nav a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    margin: 0em 3em;
+
+}
+
+nav a:hover {
+    text-decoration: underline;
+}
+
+footer {
+    color: white;
+    text-align: center;
+    margin-bottom: 6em;
+}
+
+@media only screen and (max-width: 850px) {
+
+    nav {
+    display:flex;
+    flex-direction: row;
+    padding: 2em 15em;
+    }    
+
+    nav a {
+ 
+    margin: 0em 1em;
+
+}
+
+} 
+
+
+@media only screen and (max-width: 767px) {
+    nav {
+    display:flex;
+    flex-direction: row;
+    padding: 2em 9em;
+    }    
+
+    nav a {
+
+    margin: 0em 1em;
+
+}
+
+
+
+}
+
+@media only screen and (min-width: 850px) and (max-width: 1800px) {
+    nav {
+    padding: 2em 3em;
+    }    
+
+    nav a {
+
+    margin: 0em 1em;
+
+}
+	}
+
+
+</style>
